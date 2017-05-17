3abc931cf243fc86cce4aa65239e1ac300f6d9f8
Stefan Krenz <krenz@marmalade.de>
2017-05-17 14:44:07 +0200

    Added capability to add configuration to config.inc.php

--- src/Oxrun/Application.php
+++ src/Oxrun/Application.php
@@ -26,6 +26,11 @@ class Application extends BaseApplication
     protected $autoloader;
 
     /**
+     * @var string
+     */
+    protected $oxidConfigContent;
+
+    /**
      * @param ClassLoader   $autoloader
      * @param string $name
      * @param string $version
@@ -55,14 +60,15 @@ class Application extends BaseApplication
     }
 
     /**
+     * @param bool $skipViews Add 'blSkipViewUsage' to OXIDs config.
      * @return bool
      */
-    public function bootstrapOxid()
+    public function bootstrapOxid($skipViews = false)
     {
         $input = new ArgvInput();
         if($input->getParameterOption('--shopDir')) {
             $oxBootstrap = $input->getParameterOption('--shopDir'). '/bootstrap.php';
-            if( $this->checkBootstrapOxidInclude( $oxBootstrap ) === true ) {
+            if( $this->checkBootstrapOxidInclude( $oxBootstrap, $skipViews ) === true ) {
                 return true;
             }
             return false;
@@ -72,7 +78,7 @@ class Application extends BaseApplication
         $currentWorkingDirectory = getcwd();
         do {
             $oxBootstrap = $currentWorkingDirectory . '/bootstrap.php';
-            if( $this->checkBootstrapOxidInclude( $oxBootstrap ) === true ) {
+            if( $this->checkBootstrapOxidInclude( $oxBootstrap, $skipViews ) === true ) {
                 return true;
                 break;
             }
@@ -85,14 +91,19 @@ class Application extends BaseApplication
      * Check if bootstrap file exists
      *
      * @param String $oxBootstrap Path to oxid bootstrap.php
+     * @param bool $skipViews Add 'blSkipViewUsage' to OXIDs config.
      * @return bool
      */
-    public function checkBootstrapOxidInclude($oxBootstrap)
+    public function checkBootstrapOxidInclude($oxBootstrap, $skipViews = false)
     {
         if (is_file($oxBootstrap)) {
             // is it the oxid bootstrap.php?
             if (strpos(file_get_contents($oxBootstrap), 'OX_BASE_PATH') !== false) {
                 $this->shopDir = dirname($oxBootstrap);
+
+                if ($skipViews) {
+                    $this->applyOxRunConfig(['blSkipViewUsage' => true]);
+                }
 
                 require_once $oxBootstrap;
 
@@ -106,11 +117,47 @@ class Application extends BaseApplication
                 $oModuleList = oxNew("oxModuleList");
                 $oModuleList->getModulesFromDir(\oxRegistry::getConfig()->getModulesDir());
 
+                $this->removeOxRunConfig();
+
                 return true;
             }
         }
 
         return false;
+    }
+
+    /**
+     * Adds custom Oxrun configuration to config.inc.php (if exists and not already done).
+     *
+     * @param array $config
+     */
+    protected function applyOxRunConfig(array $config = [])
+    {
+        if (null === $this->oxidConfigContent) {
+            $oxConfigInc    = "{$this->shopDir}/config.inc.php";
+            $oxConfigExists = file_exists("{$this->shopDir}/config.inc.php");
+
+            if ($oxConfigExists) {
+                $this->oxidConfigContent = file_get_contents("{$this->shopDir}/config.inc.php");
+                $newConfigContent = $this->oxidConfigContent;
+                foreach ($config as $configKey => $configValue) {
+                    $newConfigContent .= "\n\$this->{$configKey} = " . var_export($configValue, true);
+                }
+
+                file_put_contents($oxConfigInc, $newConfigContent);
+            }
+
+        }
+    }
+
+    /**
+     * Removes custom Oxrun configuration from config.inc.php.
+     */
+    protected function removeOxRunConfig()
+    {
+        if (null !== $this->oxidConfigContent) {
+            file_put_contents("{$this->shopDir}/config.inc.php", $this->oxidConfigContent);
+        }
     }
 
     /**
