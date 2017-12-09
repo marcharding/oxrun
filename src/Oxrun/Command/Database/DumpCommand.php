5b7af6f9fce63c212869fc952a935200ce0ce36f
Tobias Matthaiou <tm@loberon.de>
2017-12-09 21:50:43 +0100

    braucht man nicht mehr

--- src/Oxrun/Command/Database/DumpCommand.php
+++ src/Oxrun/Command/Database/DumpCommand.php
@@ -3,11 +3,8 @@
 namespace Oxrun\Command\Database;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
