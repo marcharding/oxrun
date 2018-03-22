e9a72e231fbdf966757c6218d83554966eeb21f0
Tobias Matthaiou <tm@loberon.de>
2018-03-22 15:12:21 +0100

    New Test: Dont Clear Compile Folder If Is Not Same Owner

--- tests/Oxrun/Command/Cache/ClearCommandTest.php
+++ tests/Oxrun/Command/Cache/ClearCommandTest.php
@@ -8,6 +8,11 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class ClearCommandTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    protected $change_back_to_origin = null;
+
     public function testExecute()
     {
         $app = new Application();
@@ -25,4 +30,53 @@ class ClearCommandTest extends TestCase
 
         $this->assertContains('Cache cleared.', $commandTester->getDisplay());
     }
+
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testDontClearCompileFolderIfIsNotSameOwner()
+    {
+        $app = new Application();
+        $app->add(new ClearCommand());
+
+        $oxconfigfile = new \oxConfigFile($app->getShopDir() . DIRECTORY_SEPARATOR . 'config.inc.php');
+        $compileDir   = $oxconfigfile->getVar('sCompileDir');
+
+        $owner = fileowner($compileDir);
+        $current_owner = posix_getuid();
+
+        if ($current_owner == $owner) {
+            $this->change_back_to_origin = ['userid' => $owner, 'path' => $compileDir];
+            chown($compileDir, 'daemon');
+            $hasChange = fileowner($compileDir);
+            if ($hasChange == $owner) {
+                throw new \PHPUnit_Framework_SkippedTestError('Process has\'t premission to change owner ');
+            };
+        }
+
+        $command = $app->find('cache:clear');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            array(
+                'command' => $command->getName()
+            )
+        );
+    }
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        if ($this->change_back_to_origin !== null) {
+            chown($this->change_back_to_origin['path'], $this->change_back_to_origin['userid']);
+            $this->change_back_to_origin = null;
+        }
+        parent::tearDown();
+    }
+
+
 }
