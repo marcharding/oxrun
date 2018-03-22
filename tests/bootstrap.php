85ff5edc8bdf36fcdfac04cd8a984bb358d2ebfa
Tobias Matthaiou <tm@loberon.de>
2018-03-22 15:59:24 +0100

    This Class can read the config.inc.php without to load OXID Framework

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,1 +1,24 @@
 <?php
+
+/**
+ * Class OxidFileConfig
+ *
+ * This Class can read the config.inc.php without to load OXID Framework
+ */
+class OxidFileConfig
+{
+    /**
+     * oxConfigFileReader constructor.
+     */
+    public function __construct()
+    {
+        $ds = DIRECTORY_SEPARATOR;
+        $oxid_base_dir = __DIR__ . "$ds..$ds..$ds";
+        // _loadConfig
+        include $oxid_base_dir . 'config.inc.php';
+        // _loadCustomConfig
+        if (file_exists($oxid_base_dir.'cust_config.inc.php')) {
+            include $oxid_base_dir . 'cust_config.inc.php';
+        }
+    }
+}
